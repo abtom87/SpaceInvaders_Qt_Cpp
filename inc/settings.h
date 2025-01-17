bce8c45b2cc5c859905d6df4075ea239d7d0d83c
Abel Thomas <abel@abel-amd5700>
2025-01-17 09:24:15 +0100

    Improved code:
    - added timer functions,
    - stopped and deleted timer upon object destruction,
    - added constant for bullet distance from below.
    - deletes alien object, stops timer on game over,
    - background image "Game Over" on game over.
    - added sanitizer address and undefined checks, thread check breaks(fix it).

--- inc/settings.h
+++ inc/settings.h
@@ -2,16 +2,16 @@
 
 #include <QSize>
 
-namespace g_vars
-{
+namespace g_vars {
 
-    constexpr QSize gCannonSize = QSize(150, 150);
+constexpr QSize gCannonSize = QSize(150, 150);
 
-    constexpr int gBulletSpeed = 7;
+constexpr int gBulletSpeed = 5;
+constexpr int gBulletDistance = 12;
 
-    constexpr int gMaxHealth = 3;
-    constexpr int gAlienSpeed = 50;
+constexpr int gMaxHealth = 3;
+constexpr int gAlienSpeed = 50;
 
-    const QString gParFolderPath = "../Resources/";
+const QString gParFolderPath = "../Resources/";
 
-}
+} // namespace g_vars
--- inc/spaceinvaders.h
+++ inc/spaceinvaders.h
@@ -1,36 +1,44 @@
 
-#include <QPointer>
 #include <QGraphicsView>
 #include <QKeyEvent>
+#include <QPointer>
+#include <QTimer>
 
 #include "components.h"
 
-class CSpaceInvaders : public QGraphicsView
-{
+class CSpaceInvaders : public QGraphicsView {
 
-    Q_OBJECT;
+  Q_OBJECT;
 
 public:
-    CSpaceInvaders(QSize oScreenSize, QWidget *pParent = nullptr);
+  CSpaceInvaders(QSize oScreenSize, QWidget *pParent = nullptr);
 
-    void Run();
+  void Run();
 
-    void CheckPoints();
+  void CheckPoints();
 
 protected:
-    void keyPressEvent(QKeyEvent *pEvent) override;
+  void keyPressEvent(QKeyEvent *pEvent) override;
 
 public slots:
-    void onCreateEnemy();
-    void onIncreaseScore();
-    void onDecreaseScore();
-    void onDecreaseHealth();
-    void onGameOver();
+  void onLaunchEnemy();
+  void onIncreaseScore();
+  void onDecreaseScore();
+  void onDecreaseHealth();
+  void onGameOver();
 
 private:
-    CCannon *m_pCannon = nullptr;
-    CPoints *m_pPoints = nullptr;
-    QSize m_oScreenSize;
-    QString m_bg_image = "../Resources/SpaceInvadersBg.jpg";
-    QString m_borderStyle = "border-style:none";
+  CCannon *m_pCannon = nullptr;
+  CPoints *m_pPoints = nullptr;
+  QSize m_oScreenSize;
+  QString m_bg_image = g_vars::gParFolderPath + "SpaceInvadersBg.jpg";
+  QString m_gameOver_image = g_vars::gParFolderPath + "gameOver.png";
+  QString m_borderStyle = "border-style:none";
+
+  QTimer *m_pTimer = nullptr;
+
+  void startTimer(uint16_t milliseconds);
+  void stopTimer();
+
+  ~CSpaceInvaders();
 };
